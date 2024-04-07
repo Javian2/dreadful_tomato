72cfa8195b6592bbdad6596c78d4299725463c23
Javi <javian235@gmail.com>
2024-04-08 00:29:48 +0200

    Add tests for navigation in home and navbar components

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,28 +1,75 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
-import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { MoviesComponent } from '../movies/movies.component';
+import { Location } from '@angular/common';
+
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ],
-      imports: [ RouterTestingModule ]
-    })
-    .compileComponents();
-  });
+  let router: any
+  let location: Location
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomeComponent ],
+      imports: [ 
+        RouterTestingModule.withRoutes([
+          { path: 'home', component: HomeComponent },
+          { path: 'movies', component: MoviesComponent },
+        ]) 
+      ]
+    })
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router)
+    location = TestBed.inject(Location)
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+  it('should have initial opacity of 0.6', () => {
+    const viewElements = fixture.debugElement.queryAll(By.css('.view'));
+  
+    for (const viewElement of viewElements) {
+      const computedStyles = window.getComputedStyle(viewElement.nativeElement);
+      expect(computedStyles.opacity).toBe('0.6');
+    }
+  });
+
+  // it('should change opacity when hover', () => {
+  //   const imgElement = fixture.debugElement.query(By.css('img'))
+  //   const viewElement = fixture.debugElement.query(By.css('.view'))
+    
+  //   imgElement.triggerEventHandler('mouseover', null);
+  //   fixture.detectChanges();
+
+  //   expect(window.getComputedStyle(viewElement.nativeElement).opacity).toBe('1')  
+  // })
+
+  it('should redirect to movies', fakeAsync(() => {
+    const moviesElement = fixture.debugElement.query(By.css('article'));
+
+    moviesElement.triggerEventHandler('click', null);
+    tick()
+
+    expect(location.path()).toBe('/movies')
+  }))
+
+  it('should redirect to series', () => {
+    const seriesElement = fixture.debugElement.queryAll(By.css('article'))[1]
+    const spy = spyOn(router, 'navigate')
+
+    seriesElement.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    expect(spy).toHaveBeenCalledWith(['series'])
+  })
+}); 
--- src/app/shared/navbar/navbar.component.spec.ts
+++ src/app/shared/navbar/navbar.component.spec.ts
@@ -1,16 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HomeComponent } from 'src/app/components/home/home.component';
+import { MoviesComponent } from 'src/app/components/movies/movies.component';
+import { SeriesComponent } from 'src/app/components/series/series.component';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
-describe('NavbarComponent', () => {
+fdescribe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let router: any
+  let location: Location
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ NavbarComponent ],
-      imports: [ RouterTestingModule ]
+      imports: [ 
+        RouterTestingModule.withRoutes([
+          { path: 'home', component: HomeComponent },
+          { path: 'movies', component: MoviesComponent },
+          { path: 'series', component: SeriesComponent },
+        ]) 
+      ]
     })
     .compileComponents();
   });
@@ -18,10 +32,162 @@ describe('NavbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router)
+    location = TestBed.inject(Location)
     fixture.detectChanges();
   });
+
+  // ------------------------------------- GENERAL AND HOME TESTS ------------------------------------- 
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not display movies and series links and filters on home page', () => {
+    component.ruta = ''
+    fixture.detectChanges()
+    
+    const navElements = fixture.debugElement.queryAll(By.css('.logo div'))
+    const filterElement = fixture.debugElement.query(By.css('.login-filter'))
+    expect(navElements).toHaveSize(0)
+    expect(filterElement).toBeFalsy()
+  })
+
+  it('should stay in home page when clicking home link button', fakeAsync(() => {
+    component.ruta = ''
+    fixture.detectChanges()
+
+    const homeButton = fixture.debugElement.query(By.css('.logo-image'))
+    homeButton.triggerEventHandler('click', null)
+    tick();
+    expect(location.path()).toBe('/home')
+  }))
+
+  
+  // ------------------------------------- MOVIES PAGE ------------------------------------- 
+
+
+  it('should display correct elements on movies component', () => {
+    component.ruta = 'movies'
+    fixture.detectChanges();
+
+    const navElements = fixture.debugElement.queryAll(By.css('.logo div'))
+    const filterElement = fixture.debugElement.query(By.css('.login-filter'))
+
+    expect(navElements).toHaveSize(2)
+    expect(filterElement).toBeTruthy();
+  })
+
+  it('should display nav items correct background colors on movies page', () => {
+    component.ruta = 'movies'
+    fixture.detectChanges();
+
+    const navElements = fixture.debugElement.queryAll(By.css('.logo div'))
+    const moviesButton = navElements[0]
+    const seriesButton = navElements[1]
+    const moviesComputedStyles = getComputedStyle(moviesButton.nativeElement)
+    const seriesComputedStyles = getComputedStyle(seriesButton.nativeElement)
+
+    expect(moviesComputedStyles.backgroundColor).toBe('rgb(255, 0, 0)')
+    expect(seriesComputedStyles.backgroundColor).toBe('rgb(0, 0, 0)')
+  })
+
+  it('should navigate to home when clicking home button on movies page', fakeAsync(() => {
+    component.ruta = 'movies'
+    fixture.detectChanges();
+
+    const homeButton = fixture.debugElement.query(By.css('.logo-image'))
+    homeButton.triggerEventHandler('click', null)
+    tick()
+    expect(location.path()).toBe('/home')
+  }))
+
+  it('should navigate to series when clicking series button on movies page', fakeAsync(() => {
+    component.ruta = 'movies'
+    fixture.detectChanges();
+
+    const navElements = fixture.debugElement.queryAll(By.css('.logo div'))
+
+    navElements[1].triggerEventHandler('click', null)
+    tick()
+    expect(location.path()).toBe('/series')
+  }))
+
+  it('should stay in movies page when clicking movies button', fakeAsync(() => {
+    component.ruta = 'movies'
+    fixture.detectChanges();
+
+    const navElements = fixture.debugElement.queryAll(By.css('.logo div'))
+    const moviesButton = navElements[0]
+    moviesButton.triggerEventHandler('click', null)
+    tick()
+    expect(location.path()).toBe('/movies')
+
+    const moviesComputedStyles = getComputedStyle(moviesButton.nativeElement)
+    expect(moviesComputedStyles.backgroundColor).toBe('rgb(255, 0, 0)')
+  }))
+
+
+  // ------------------------------------- SERIES PAGE ------------------------------------- 
+
+
+  it('should display correct elements on series component', () => {
+    component.ruta = 'series'
+    fixture.detectChanges();
+
+    const navElements = fixture.debugElement.queryAll(By.css('.logo div'))
+    const filterElement = fixture.debugElement.query(By.css('.login-filter'))
+
+    expect(navElements).toHaveSize(2)
+    expect(filterElement).toBeTruthy();
+  })
+
+  it('should display nav items correct background colors on series page', () => {
+    component.ruta = 'series'
+    fixture.detectChanges();
+
+    const navElements = fixture.debugElement.queryAll(By.css('.logo div'))
+    const moviesButton = navElements[0]
+    const seriesButton = navElements[1]
+    const moviesComputedStyles = getComputedStyle(moviesButton.nativeElement)
+    const seriesComputedStyles = getComputedStyle(seriesButton.nativeElement)
+
+    expect(moviesComputedStyles.backgroundColor).toBe('rgb(0, 0, 0)')
+    expect(seriesComputedStyles.backgroundColor).toBe('rgb(255, 0, 0)')
+  })
+
+  it('should navigate to home when clicking home button on series page', fakeAsync(() => {
+    component.ruta = 'series'
+    fixture.detectChanges();
+
+    const homeButton = fixture.debugElement.query(By.css('.logo-image'))
+    homeButton.triggerEventHandler('click', null)
+    tick()
+    expect(location.path()).toBe('/home')
+  }))
+
+  it('should navigate to movies when clicking movies button on series page', fakeAsync(() => {
+    component.ruta = 'series'
+    fixture.detectChanges();
+
+    const navElements = fixture.debugElement.queryAll(By.css('.logo div'))
+
+    navElements[0].triggerEventHandler('click', null)
+    tick()
+    expect(location.path()).toBe('/movies')
+  }))
+
+  it('should stay in series page when clicking series button', fakeAsync(() => {
+    component.ruta = 'series'
+    fixture.detectChanges();
+
+    const navElements = fixture.debugElement.queryAll(By.css('.logo div'))
+    const seriesButton = navElements[1]
+    seriesButton.triggerEventHandler('click', null)
+    tick()
+    expect(location.path()).toBe('/series')
+
+    const seriesComputedStyles = getComputedStyle(seriesButton.nativeElement)
+    expect(seriesComputedStyles.backgroundColor).toBe('rgb(255, 0, 0)')
+  }))
 });
